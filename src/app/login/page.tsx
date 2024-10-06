9f2ad48654ba72b56153bc2aee06378621198914
Md. Mansur <ahammadullah2001@gmail.com>
2024-10-07 00:48:53 +0600

    nest auth client

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,9 +1,12 @@
 "use client";
+import { loginUser } from "@/utils/actions/loginUser";
+import { signIn } from "next-auth/react";
 import Image from "next/image";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 import { useForm } from "react-hook-form";
 
-type FormValues = {
+export type FormValues = {
   email: string;
   password: string;
 };
@@ -15,8 +18,23 @@ const LoginPage = () => {
     formState: { errors },
   } = useForm<FormValues>();
 
+  const router = useRouter();
   const onSubmit = async (data: FormValues) => {
     console.log(data);
+    try {
+      const res = await loginUser(data);
+
+      if (res.accessToken) {
+        alert(res.message);
+        localStorage.setItem("accessToken", res.accessToken);
+        router.push("/");
+      }
+
+      console.log(res);
+    } catch (err: any) {
+      console.error(err.message);
+      throw new Error(err.message);
+    }
   };
 
   return (
@@ -57,7 +75,7 @@ const LoginPage = () => {
               <input
                 {...register("password")}
                 type="password"
-                placeholder="Email"
+                placeholder="Password"
                 className="input input-bordered"
                 required
               />
@@ -77,7 +95,14 @@ const LoginPage = () => {
           </form>
           <p className="text-center">Or Sign Up Using</p>
           <div className="flex justify-center mb-10 mt-2">
-            <button className="btn btn-circle ">
+            <button
+              onClick={() =>
+                signIn("google", {
+                  callbackUrl:
+                    "https://next-auth-client-two.vercel.app/dashboard",
+                })
+              }
+              className="btn btn-circle ">
               <Image
                 src="https://www.freepnglogos.com/uploads/google-logo-png/google-logo-png-webinar-optimizing-for-success-google-business-webinar-13.png"
                 width={50}
@@ -85,7 +110,14 @@ const LoginPage = () => {
                 alt="google logo"
               />
             </button>
-            <button className="btn btn-circle">
+            <button
+              className="btn btn-circle"
+              onClick={() =>
+                signIn("github", {
+                  callbackUrl:
+                    "https://next-auth-client-two.vercel.app/dashboard",
+                })
+              }>
               <Image
                 src="https://cdn-icons-png.flaticon.com/512/25/25231.png"
                 width={35}
--- src/components/shared/Navbar.tsx
+++ src/components/shared/Navbar.tsx
@@ -1,6 +1,15 @@
+"use client";
+import { signOut } from "next-auth/react";
 import Link from "next/link";
 
-const Navbar = () => {
+type UserPros = {
+  user: {
+    name?: string | undefined;
+    email?: string | undefined;
+    image?: string | undefined;
+  };
+};
+const Navbar = ({ session }: { session: UserPros | null }) => {
   return (
     <div className="navbar bg-base-100  border-b  w-[90%] mx-auto">
       <div className="navbar-start">
@@ -11,8 +20,7 @@ const Navbar = () => {
               className="h-5 w-5"
               fill="none"
               viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
+              stroke="currentColor">
               <path
                 strokeLinecap="round"
                 strokeLinejoin="round"
@@ -23,8 +31,7 @@ const Navbar = () => {
           </div>
           <ul
             tabIndex={0}
-            className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
-          >
+            className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
             <li>
               <Link href="/">Home</Link>
             </li>
@@ -59,16 +66,21 @@ const Navbar = () => {
         </ul>
       </div>
       <div className="navbar-end">
-        <button className="btn btn-error btn-outline text-white rounded-full px-5">
-          Logout
-        </button>
-
-        <Link
-          href="/login"
-          className="btn btn-accent btn-outline text-white rounded-full px-5"
-        >
-          Login
-        </Link>
+        {session?.user ? (
+          <button
+            onClick={() => {
+              signOut();
+            }}
+            className="btn btn-error btn-outline text-white rounded-full px-5">
+            Logout
+          </button>
+        ) : (
+          <Link
+            href="/login"
+            className="btn btn-accent btn-outline text-white rounded-full px-5">
+            Login
+          </Link>
+        )}
       </div>
     </div>
   );
